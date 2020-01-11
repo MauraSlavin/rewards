5a940f3d00925cbc0aefcf6935c708e79dbfd0a4
Maura Slavin <MauraMSlavin@gmail.com>
2020-01-11 16:00:37 -0500

    Changed seeds to match what Sophie is using in the html files.

--- database/seeds.sql
+++ database/seeds.sql
@@ -2,51 +2,75 @@
 -- but if you don't know what the id is going to be, you risk getting an error when inserting a foreign id,
 -- so I'm inserting id's so I'll know what they are for inserting seed data for testing.
 
+
 -- only uses one parent.  Needed for email address (and alt_email, if we get to that)
 DELETE FROM parents;
 INSERT INTO parents (id, name, email) VALUES (1, 'Andrew', 'Andy@gmail.com');
 
+
 -- only one child for first iteration.  DB is built to accommodate multiple children in future releases.
 DELETE FROM children;
--- in these seeds, Sophia swept the floor (10 pts) & took out the trash (15 pts) and got a cookie (-10 pts), so has 15 points left
-INSERT INTO children (id, name, email, points) VALUES (1, 'Sophia', 'S@gmail.com', 15);
+-- in these seeds, Sophia did 220 worth of chores, and spent 175 chores for a balance of 45 point.
+INSERT INTO children (id, name, email, points) VALUES (1, 'Sophia', 'S@gmail.com', 45);
  
+
 -- Put a few chores in the db
 DELETE FROM chores;
 INSERT INTO chores (id, title, points, iconfile) VALUES
-	(1, 'Fold laundry', 10, 'folding-clothes.png'),
-	(2, 'Sweep floor', 10, 'broom.png'),
-	(3, 'Take trash out', 15, 'trash.png'),
-	(4, 'Clear dinner table', 5, 'table.png');
+	(1, 'Bathe dog', 10, 'bath_dog.png'),
+	(2, 'Workbook', 10, 'do_worbook.png'),
+	(3, 'Watering', 10, 'watering.png'),
+	(4, 'Pickup toys', 10, 'pickup_toys (1).png'),
+    (5, 'Fish bowl', 10, 'fishbowl.png'),
+    (6, 'Sweep room', 10, 'broom.png'),
+    (7, 'Fold cloths', 20, 'folding-clothes.png'),
+    (8, 'Make bed', 20, 'make_bed.png'),
+    (9, 'Read book', 20, 'reading-book.png'),
+    (10, 'Set table', 30, 'set_table_2.png'),
+    (11,'Clear table', 30, 'table.png'),
+    (12, 'Trash out', 30, 'trash.png'),
+    (13, 'Vacuum', 40, 'vacuum.png'),
+    (14, 'Walk dog', 40, 'walk_dog.png'),
+    (15, 'Wash dishes', 40, 'wash_dish.png')
+    ;
+
 
 -- put a few rewards in the db
 DELETE FROM rewards;
 INSERT INTO rewards (id, title, points, iconfile) VALUES
-	(1, '$25 Cash', 250, 'salary (1).png'),
-	(2, 'Out to the movies', 150, 'cinema.png'),
-	(3, 'Reading a book with a parent', 100, 'reading-book.png'),
-    (4, 'Family game night', 15, 'family.png'),
-	(5, 'An hour of screen time', 60, 'computer.png');
+	(1, 'Movie Night', 100, 'cinema.png'),
+	(2, 'Screen Time', 150, 'computer.png'),
+	(3, 'Family Time', 175, 'family.png'),
+    (4, '$$ Money $$', 300, 'salary (1).png'),
+	(5, 'Playdate', 250, 'playdate.png');
 
- -- put a few assignments in the db
+
+-- put a few assignments in the db
 DELETE FROM assignedchores;
 INSERT INTO assignedchores (id, ChoreId, ChildId) VALUES
-	(1, 2, 1),   -- chore #2, sweep floor, assigned to child 1 (10 pts)
-    (2, 2, 1),   -- chore #2, sweep floor, assigned to child 1 (10 pts) (a second time)
-	(3, 3, 1);   -- chore #3, take trash out, assigned to child 1 (15 pts)
-
--- assume a few chores were done
+	(1, 8, 1),   -- chore #8, make bed, assigned to child 1 (20 pts)
+    (2, 10, 1),   -- chore #10, Set table, assigned to child 1 (30 pts)
+	(3, 11, 1);   -- chore #11, Clear table, assigned to child 1 (30 pts)
+    
+    
+-- put a few chore completed in the database
 DELETE FROM donechores;
 INSERT INTO donechores (id, ChildId, ChoreId) VALUES
-	(1, 1, 2),    -- chore #2, sweep floor, was done by child 1 (10 pts)
-	(2, 1, 3);    -- chore #3, trash out, was done by child 1 (15 pts)
+	(1, 1, 7),   -- chore #7, fold, assigned to child 1 (20 pts)
+    (2, 1, 9),   -- chore #9, read, assigned to child 1 (20 pts) (a second time)
+	(3, 1, 11),   -- chore #11, clear table, assigned to child 1 (30 pts)
+    (4, 1, 13),   -- chore #13, vacuum, done by to child 1 (40 pts)
+    (5, 1, 12),   -- chore #12, trash out, done by to child 1 (30 pts)
+    (6, 1, 14),   -- chore #14, walk dog, done by to child 1 (40 pts)    
+    (7, 1, 13);   -- chore #13, vacuum (again), done by to child 1 (40 pts) 
     
 
 -- and a reward was given    
 DELETE FROM usedpoints;    
 INSERT INTO usedpoints (id, ChildId, RewardId) VALUES
-	(1, 1, 4);  -- child 1 got reward #4 (cookie for 10 points)
+	(1, 1, 3);  -- child 1 got reward #3 (family time for 175 points)
     
+
 -- Verify each table has what you expect it to.
 SELECT * FROM parents;
 SELECT * FROM children;
