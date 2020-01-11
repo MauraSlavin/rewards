4cd2f8db74fa60d47ae799ecfba4c6750faf4974
David Hamilton <dahamilton10@yahoo.com>
2020-01-11 10:29:45 -0500

    fixed some linting errors

--- models/chore.js
+++ models/chore.js
@@ -1,22 +1,22 @@
 module.exports = (sequelize, DataTypes) => {
-  const Chore = sequelize.define("Chore", {
+  const Chore = sequelize.define('Chore', {
     title: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [1]
-      }
+        len: [1],
+      },
     },
     points: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      defaultValue: 0
+      defaultValue: 0,
     },
     iconfile: {
       type: DataTypes.STRING,
       allowNull: false,
-      defaultValue: "wash_dish.png"
-    }
+      defaultValue: 'wash_dish.png',
+    },
   });
   return Chore;
 };
